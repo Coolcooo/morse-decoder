f28e624ea6ceef72d0294029d291142569a76d8f
Vladimir Privezenov <privezenoff@yandex.ru>
2020-10-07 00:17:45 +0300

    feat: add resolve morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,8 +38,35 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let sortedMorseTable = {};
+    const sorted = Object.keys(MORSE_TABLE).sort((a, b) => {
+        return a.length - b.length;
+    })
+    for (let key of sorted) {
+        let d = key.split('').map( e=> {
+            if (e=== '.') {
+                return '10'
+            } else {
+                return '11'
+            }
+        }).join('')
+        sortedMorseTable[d] = MORSE_TABLE[key];
+    }
+    sortedMorseTable['**********'] = ' ';
+    let c = [];
+    for(let i = 0;i < expr.length;i+=10) {
+        c.push(expr.slice(i,i+10))
+    }
+     return c.map(e => {
+        for (let i of Object.keys(sortedMorseTable).reverse()) {
+            if (e.includes(i)) {
+                return sortedMorseTable[i];
+            }
+        }
+    }).join('')
+
+    }
+
 
 module.exports = {
     decode
